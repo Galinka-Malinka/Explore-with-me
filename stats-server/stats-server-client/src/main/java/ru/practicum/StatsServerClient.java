9a05a2539a98c0dcf078ed85749858980fcac3ff
Galina <Galya_iv@mail.ru>
2023-08-26 21:01:20 +0300

    успешная настройка многомодульного проекта

---
+++ stats-server/stats-server-client/src/main/java/ru/practicum/StatsServerClient.java
@@ -0,0 +1,11 @@
+package ru.practicum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+public class StatsServerClient {
+    public static void main(String[] args) {
+        SpringApplication.run(StatsServerClient.class, args);
+    }
+}
